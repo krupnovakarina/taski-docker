842f524941033ace20dc32eb3ac346f5ff1c2533
Karina Krupnova <krupnova.karina@icloud.com>
2025-06-20 19:26:14 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,12 +3,16 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['123.123.123.123', 'localhost', 'yasha.hopto.org', '127.0.0.1']
+ALLOWED_HOSTS = [
+    '123.123.123.123',
+    'localhost',
+    'yasha.hopto.org',
+    '127.0.0.1'
+]
 
 
 # Application definition
